1bd59476b8f84cda2b0bf5c8db47542333113990
doulab <doulab777@gmail.com>
2016-08-12 11:42:16 +0000

    Add try catch on method

--- showcustomtoast/src/main/java/com/sensoftsarl/showcustomtoast/ShowToastUtil.java
+++ showcustomtoast/src/main/java/com/sensoftsarl/showcustomtoast/ShowToastUtil.java
@@ -13,37 +13,56 @@ import android.widget.Toast;
  */
 public class ShowToastUtil extends Activity {
 
+    private static String TAG = ShowToastUtil.class.getSimpleName();
     // Error toast
     public static void error(Context context, String message) {
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View view =inflater.inflate(R.layout.error_toast, null);
-        TextView textView = (TextView) view.findViewById(R.id.textView);
-        textView.setText(message);
-        showCostomToast(context,view);
+        try {
+
+            LayoutInflater inflater = LayoutInflater.from(context);
+            View view = inflater.inflate(R.layout.error_toast, null);
+            TextView textView = (TextView) view.findViewById(R.id.textView);
+            textView.setText(message);
+            showCostomToast(context, view);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
     // Warming toast
     public static void warming(Context context,String message) {
+        try{
         LayoutInflater inflater = LayoutInflater.from(context);
         View view =inflater.inflate(R.layout.warming_toast, null);
         TextView textView = (TextView) view.findViewById(R.id.textView);
         textView.setText(message);
         showCostomToast(context,view);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
     // Info toast
     public static void info(Context context,String message) {
+        try{
         LayoutInflater inflater = LayoutInflater.from(context);
         View view =inflater.inflate(R.layout.info_toast, null);
         TextView textView = (TextView) view.findViewById(R.id.textView);
         textView.setText(message);
         showCostomToast(context,view);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public static void showCostomToast(Context context,View v){
+        try{
         Toast customtoast=new Toast(context);
         customtoast.setView(v);
         customtoast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL,0, 0);
         customtoast.setDuration(Toast.LENGTH_LONG);
         customtoast.show();
+        }catch (Exception e){
+           e.printStackTrace();
+        }
     }
 
 }
